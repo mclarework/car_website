4221abcb736535579f8015202f45847b13b237a0
mclarework <59828756+mclarework@users.noreply.github.com>
2021-05-14 11:02:52 +0100

    Fixed JS file

--- index.js
+++ index.js
@@ -1,46 +1,17 @@
 let button = document.getElementById("button")
 let car = document.getElementById("car")
-let x = 0
-let positionX = car.style.transform = `translate(${(toString(x))}px)`
 
 
-// const drive = () => {
-//     if (position == "0px") {
-//         position = "1100px"
-//         car.style.transform = `translate(${position})`
-//     }else if (position == "1100px") {
-//         position = "0px"
-//         car.style.transform = `translate(${position})`
-//     }
-//     car.style.transition = "2s"
-//     car.style.transitionTimingFunction = "ease-in-out"
-
-// }
-
-// button.addEventListener("click", ()=>{drive()})
 
 
-const checkKey = (e) => {
-    
-    e = e || window.event;
-    
-    if (e.keyCode == '38') {
-        // up arrow
-    }
-    else if (e.keyCode == '40') {
-        // down arrow
-    }
-    else if (e.keyCode == '37') {
-        // left arrow
-    }
-    else if (e.keyCode == '39') {
-        // right arrow
-        car.style.transform = `translate(${(toString(position+1))}px)`
-        console.log("right")
-        
-    }
-    
+const drive = () => {
+    position = "1100px"
+    car.style.transform = `translate(${position})`
+    car.style.transition = "2s"
+    car.style.transitionTimingFunction = "ease-in-out"
+
 }
 
+button.addEventListener("click", ()=>{drive()})
 
-document.onkeydown = checkKey;
+
